cce757f987f9a02da40d3c212d74d712aa84b650
Yoko Kasai <ykookaasi@gmail.com>
2020-11-12 16:35:42 +0100

    Implement all user actions

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,36 @@
+class DosesController < ApplicationController
+  before_action :find_dose, only: [:destroy]
+
+  def new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose = Dose.new
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @cocktail = @dose.cocktail
+    @dose.destroy
+
+    redirect_to cocktail_path(@cocktail)
+  end
+
+  private
+
+  def find_dose
+    @dose = Dose.find(params[:id])
+  end
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Destroying all your cocktails'
+
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+puts "Creating new"
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "gin and tonic")
+Cocktail.create(name: "mai tai")
+Cocktail.create(name: "mojito")
+
+puts "Created #{Cocktail.count} cocktails!"
