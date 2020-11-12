0739f60fdb9b930e7d8bc6e4f62495510f06bcb8
Yoko Kasai <ykookaasi@gmail.com>
2020-11-12 17:00:04 +0100

    Add homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
+  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-
+  root to: 'pages#home'
   resources :cocktails, only: [:index, :create, :new, :show, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
