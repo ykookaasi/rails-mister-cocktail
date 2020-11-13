3d23f3eff005987ef6ff9a8dcdb2cfcf0414bcf7
Yoko Kasai <ykookaasi@gmail.com>
2020-11-13 15:12:43 +0100

    Change root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'pages/home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  root to: 'pages#home'
+  root to: 'cocktails#index'
   resources :cocktails, only: [:index, :create, :new, :show, :create, :destroy] do
     resources :doses, only: [:new, :create]
   end
